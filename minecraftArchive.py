910986f022756c7346d786b4106aa645c73de5d0
Eddy Hintze <eddy.hintze@gmail.com>
2014-09-15 19:11:17 -0400

    script now makes Minecraft dir

--- minecraftArchive.py
+++ minecraftArchive.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import re
+import os
 import urllib2
 import urllib
 
@@ -8,18 +9,17 @@ request = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
 page = urllib2.urlopen( request )
 soup = BeautifulSoup(page.read())
 
+os.mkdir("Minecraft")
 
 mcReleases = soup.findAll('div',{'class':'box'})
-
-
-for mcRelease in mcReleases:
+for mcRelease in mcReleases: #loop through The release types
 	releaseName = mcRelease['id']
 	mcVersions = mcRelease.findAll('div',{'class':'release'})
-	for mcVersion in mcVersions:
+	for mcVersion in mcVersions: # loop through each version in that release
 		dlLinks = mcVersion.findAll('a')
 		verison = mcVersion.find('h3')
 		releaseDate = mcVersion.find('span',{'class':'time'})
-		for link in dlLinks:
+		for link in dlLinks: #download both the client and server files
 			fixedDate = releaseDate.string.split('/')
 			fixedDate = fixedDate[2]+"."+fixedDate[0]+"."+fixedDate[1]
 			jarFile = urllib.URLopener()
@@ -32,5 +32,4 @@ for mcRelease in mcReleases:
 			print link['href']
 			print "\n"
 
-	#print mcRelease
 	print "\n\n"
